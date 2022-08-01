4856f58c38f42451bed8c0b43a8a346845120fc6
Lihuel Seifert <lihuelemiliozzi@gmail.com>
2022-08-01 14:07:19 -0300

    SQL_funcional

--- familiares/familia/models.py
+++ familiares/familia/models.py
@@ -1,3 +1,9 @@
+from errno import EDEADLK
 from django.db import models
+from django.forms import CharField
 
 # Create your models here.
+class familias(models.Model):
+    nombre = models.CharField(max_length=50)
+    edad = models.IntegerField()
+    ciudad = models.CharField(max_length=50)
