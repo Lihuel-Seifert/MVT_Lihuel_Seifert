de1237ef94e2eb0ddd83f7ade418d720a73b0a9b
Lihuel Seifert <lihuelemiliozzi@gmail.com>
2022-08-01 16:28:49 -0300

    MVT_Lihuel_Seifert

--- familiares/familia/views.py
+++ familiares/familia/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from familia.models import Familias
 from django.template import Template, Context, loader
+from datetime import datetime #importe el datetime
 
 # Create your views here.
 
@@ -11,7 +12,7 @@ def index(request):
 
     familiares = Familias.objects.all()
 
-    familiares = {'familiares':familiares}
+    familiares = {'familiares':familiares, 'datetame': datetime.now()} # Agrego al datetime  al dicionario
 
     plantilla = loader.get_template("index.html")
 
